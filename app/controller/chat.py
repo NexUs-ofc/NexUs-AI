6c4de5badeb14665b9e18b687e3ddc1b1c7f961c
BrunaOliveira14 <bruna.jesus@germinare.org.br>
2026-09-21 12:27:45 -0300

    add line

--- app/controller/chat.py
+++ app/controller/chat.py
@@ -8,6 +8,8 @@ from ..auth.dependencies import get_current_user
 from ..model.dto.chat_request import ChatRequest
 from ..model.dto.chat_response import ChatResponse
 from ..repository.mongodb.conversations import ConversationsRepository
+
+
 router = APIRouter(prefix="/chat")
 
 @router.post("/", response_model=ChatResponse)
